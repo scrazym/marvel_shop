01e3981fd2341bf03a20beafc15a52f32ccbfdf2
scrazym <mil.grof@mail.ru>
2023-10-19 10:40:46 +0300

    refactor: create new directory for git

---
+++ src/components/charList/CharList.js
@@ -0,0 +1,134 @@
+import { Component } from "react";
+
+import "./charList.scss";
+
+import MarvelService from "../../services/MarvelService";
+import Spinner from "../spinner/Spinner";
+import ErrorMessage from "../errorMessage/ErrorMessage";
+
+class CharList extends Component {
+  state = {
+    charList: [],
+    loading: true,
+    error: false,
+    newItemLoading: false,
+    offset: 210,
+    charEnded: false,
+  };
+
+  marvelService = new MarvelService();
+
+  componentDidMount() {
+    this.onRequest();
+  }
+
+  onRequest = (offset) => {
+    this.onCharListLoading();
+    this.marvelService
+      .getAllCharacters(offset)
+      .then(this.onCharListLoaded)
+      .catch(this.onError);
+  };
+
+  onCharListLoading = () => {
+    this.setState({
+      newItemLoading: true,
+    });
+  };
+
+  onCharListLoaded = (newCharList) => {
+    let ended = false;
+    if (newCharList.length < 9) {
+      ended = true;
+    }
+
+    this.setState(({ offset, charList }) => ({
+      charList: [...charList, ...newCharList],
+      loading: false,
+      newItemLoading: false,
+      offset: offset + 9,
+      charEnded: ended,
+    }));
+  };
+
+  onError = () => {
+    this.setState({ loading: false, error: true });
+  };
+
+  itemRefs = [];
+
+  setRef = (ref) => {
+    this.itemRefs.push(ref);
+  };
+
+  focusOnItem = (id) => {
+    this.itemRefs.forEach((item) =>
+      item.classList.remove("char__item_selected")
+    );
+    this.itemRefs[id].classList.add("char__item_selected");
+    this.itemRefs[id].focus();
+  };
+  renderChars(list) {
+    const items = list.map((item, i) => {
+      let imgStyle = { objectFit: "cover" };
+      if (
+        item.thumbnail ===
+        "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+      ) {
+        imgStyle = { objectFit: "unset" };
+      }
+
+      return (
+        <li
+          className="char__item"
+          tabIndex={0}
+          ref={this.setRef}
+          key={item.id}
+          onClick={() => {
+            this.props.onCharSelected(item.id);
+            this.focusOnItem(i);
+          }}
+          onKeyDown={(e) => {
+            if (e.key === " " || e.key === "Enter") {
+              this.props.onCharSelected(item.id);
+              this.focusOnItem(i);
+            }
+          }}
+        >
+          <img src={item.thumbnail} alt={item.name} style={imgStyle} />
+          <div className="char__name">{item.name}</div>
+        </li>
+      );
+    });
+    return <ul className="char__grid">{items}</ul>;
+  }
+
+  render() {
+    const { charList, loading, error, newItemLoading, offset, charEnded } =
+      this.state;
+
+    const items = this.renderChars(charList);
+
+    const errorMessage = error ? <ErrorMessage /> : null;
+    const spinner = loading ? <Spinner /> : null;
+    const content = !(loading || error) ? items : null;
+
+    return (
+      <div className="char__list">
+        {errorMessage}
+        {spinner}
+        {content}
+        <button
+          className="button button__main button__long"
+          disabled={newItemLoading}
+          onClick={() => this.onRequest(offset)}
+          style={{ display: charEnded ? "none" : "block" }}
+        >
+          <div className="inner">load more</div>
+        </button>
+      </div>
+    );
+  }
+}
+
+export default CharList;
---
+++ src/components/errorMessage/ErrorMessage.js
@@ -0,0 +1,7 @@
+import img from "./error.gif";
+
+const ErrorMessage = () => {
+  return <img className="error__img" src={img} alt="error gif" />;
+};
+
+export default ErrorMessage;
