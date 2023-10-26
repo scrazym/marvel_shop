1abf0f02a176fcd55aafaee4b15a65e1fe16370b
scrazym <mil.grof@mail.ru>
2023-10-26 18:32:20 +0300

    feat: add routes

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,39 +1,18 @@
-import { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+import { ComicsPage, MainPage } from "../pages";
 import AppHeader from "../appHeader/AppHeader";
-import AppBanner from "../appBanner/AppBanner";
-import ComicsList from "../comicsList/ComicsList";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-
-import decoration from "../../resources/img/vision.png";
 
 const App = () => {
-  const [selectedChar, setChar] = useState(null);
-
-  const onCharSelected = (id) => {
-    setChar(id);
-  };
-
   return (
     <Router>
       <div className="app">
         <AppHeader />
         <main>
-          {/* <RandomChar />
-        <div className="char__content">
-          <CharList onCharSelected={onCharSelected} />
-          <ErrorBoundary>
-            <CharInfo charId={selectedChar} />
-          </ErrorBoundary>
-        </div>
-        <img className="bg-decoration" src={decoration} alt="vision" /> */}
-          <AppBanner />
-
-          <ComicsList />
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/comics" element={<ComicsPage />} />
+          </Routes>
         </main>
       </div>
     </Router>
